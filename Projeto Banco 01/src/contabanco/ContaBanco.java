e820556e8486d1f149a2dcdbfe6b90bb1f7222fa
Leonardo Alves <104588087+Zhorg10@users.noreply.github.com>
2023-04-18 16:36:08 -0300

    Commit projeto de estudo banco 01

--- Projeto Banco 01/src/contabanco/ContaBanco.java
+++ Projeto Banco 01/src/contabanco/ContaBanco.java
@@ -1,13 +1,12 @@
 package contabanco;
 
-
 public class ContaBanco {
 	public int numConta;
 	protected String tipo;
 	private String dono;
 	private float saldo;
 	private boolean status;
-	
+
 	public ContaBanco() {
 		super();
 		this.setSaldo(0);
@@ -53,34 +52,47 @@ public class ContaBanco {
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
-	
+
 	public void abirConta() {
-		if (this.getTipo() == "cc" && this.getTipo() != "cp") {
-			this.setSaldo(50);
+		if (this.numConta > 0 && this.tipo != null && this.dono != null && status == false) {
+			if (this.tipo != null || this.tipo != null) {
+				if (this.tipo.equals("cc"))
+					setSaldo(50);
+				System.out.println("Conta Nº " + this.numConta + ", criada com sucesso!");
+				System.out.println("--------Dados básicos--------");
+				System.out.println("Cliente: " + this.dono);
+				System.out.println("Tipo de conta: " + (this.tipo.equals("cc") ? "Conta Corrente" : "Conta Poupança"));
+				System.out.println("Saldo disponível: " + this.saldo);
+				setStatus(true);
+				System.out.println("Status da conta: " + (this.status ? "Ativada" : "Desativada"));
+			}
+		} else {
+			System.out.println(
+					"Não foi possível a abertura da conta, favor verficar o número, tipo e responsável da conta!");
 		}
+
 	}
-	
+
 	public void fecharConta() {
-		if(this.getSaldo() < 0) {
+		if (this.getSaldo() < 0) {
 			System.out.println("ERRO: Você não pode fechar sua conta pois existem débitos pendentes.");
-		} else if(this.getSaldo() >= 0) {
+		} else if (this.getSaldo() >= 0) {
 			this.setStatus(false);
 		}
 	}
-	
+
 	public void depositar(float valor) {
-		if(valor > 0) {
-			
+		if (valor > 0) {
+
 		}
 	}
-	
+
 	public void sacar() {
-		
+
 	}
-	
+
 	public void pagarMensalidade() {
-		
+
 	}
-	
-	
+
 }
--- Projeto Banco 01/src/contabanco/Index.java
+++ Projeto Banco 01/src/contabanco/Index.java
@@ -1,10 +1,85 @@
 package contabanco;
 
+import java.util.Scanner;
+
 public class Index {
 
 	public static void main(String[] args) {
-		ContaBanco cc1 = new ContaBanco();
 		
+		System.out.println("Bem vindo ao sistema bancário pessoal, selecione uma das opções para atendimento:");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("1: Status da conta");
+		System.out.println("2: Consultar saldo em conta");
+		System.out.println("3: Abrir conta");
+		System.out.println("4: Sacar");
+		System.out.println("5: Depositar");
+		System.out.println("6: Pagar mensalidade");
+		System.out.println("7: Encerrar minha conta");
+		
+		System.out.print("Digite aqui: ");
+		int num = scan.nextInt();
+
+		switch (num) {
+		case 1:
+			System.out.println("Ainda não existe conta.");
+			break;
+		case 2:
+			System.out.println("Ainda não existe saldo em conta disponível");
+			break;
+		case 3:
+			System.out.println("Para a abertura da conta gentileza informar o número da conta, o tipo da conta e o nome do responsável.");
+			int numConta;
+			String tipo;
+			String dono;
+			System.out.print("Número da conta: ");
+			numConta = scan.nextInt();
+			System.out.println();
+			
+			
+			System.out.println("Favor inserir 'cc' para conta corrente e 'cp' para conta poupança");
+			System.out.print("Tipo da conta : ");
+			tipo = scan.next();
+			System.out.println();
+			
+			
+			System.out.print("Responsável pela conta: ");
+			dono = scan.next();
+			System.out.println();
+			
+		
+			ContaBanco cc1 = new ContaBanco();
+			cc1.setNumConta(numConta);
+			cc1.setTipo(tipo);
+			cc1.setDono(dono);
+			cc1.abirConta();
+
+			break;
+		case 4:
+			System.out.println("sacar");
+			break;
+		case 5:
+			System.out.println("depositar");
+			break;
+		case 6:
+			System.out.println("pagar mes");
+			break;
+		case 7:
+			System.out.println("encerrar");
+			break;
+		default:
+			System.out.println("Número inválido, informe um número dentro das opções desejadas");
+			break;
+		}
+		System.exit(0);
+		
+		
+		
+		
+		scan.close();
+
+
 	}
 
 }
